52046d95889e0212c8889b83d72e3b6c4b97e6c1
woufs <897213449@qq.com>
2022-07-24 17:41:52 +0800

    2

---
+++ Project2/Project2/源.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int d = 4;
+//int main()
+//{
+//	int a;
+//	int b;
+//	int c;
+//	
+//    scanf("%d%d", &a ,&b);
+//	c = a + b+d;
+//	printf("c=%d", c);
+//	
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	{int a = 5;
+//	printf("%d\n", a);
+//
+//	
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a=4;
+//	printf("3",4);
+//	scanf
+//}
+//int main()
+//{
+//	char arr[] = "hello";
+//	printf(arr);
+//
+//
+//	return 0;
+//}
+int main()
+{
+	int a = 5;
+	printf("%d",a);
+	return 0;
+}
