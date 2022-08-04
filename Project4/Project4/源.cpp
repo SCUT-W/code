d761c4650b19776b8817326733c0b1a97446dcbc
woufs <897213449@qq.com>
2022-08-04 18:03:13 +0800

    ...

---
+++ Project4/Project4/源.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//
+//int main()
+//{
+//	char arr2[] = { 'b', 'i', 't' £¬};
+//	char arr3[] = { 'b', 'i', 't'£¬'\0' £¬};
+//
+//	printf("%s", arr2);
+//	return 0;
+//}
+//int main()
+//{
+//    char arr1[] = "bit3";
+//    char arr2[] = { 'b', 'i', 't' };
+//    char arr3[] = { 'b','i','t','\0' };
+// 
+//    printf("%s\n", arr1);
+//    printf("%s\n", arr2);
+//  
+//    printf("%s\n", arr3);
+//   
+//  
+//    return 0;
+//}int
+int main()
+{
+	printf("\328");
+
+
+	return 0;
+}
